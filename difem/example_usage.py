56c8b99f2e9890484ba14b71ece431d7beafb6ee
unknown <shaimaaalmously054@gmail.com>
2025-12-24 12:06:25 +0300

    Add Demo and delet unused file

--- difem/example_usage.py
+++
@@ -1,8 +0,0 @@
-from difem.feature_extraction import extract_difem_features
-
-features = extract_difem_features(
-    "data/sample_video_json"
-)
-
-print("DIFEM feature vector:", features)
-print("Feature dimension:", features.shape)
